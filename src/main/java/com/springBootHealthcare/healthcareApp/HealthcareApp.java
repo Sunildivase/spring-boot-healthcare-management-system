469a2a2f5f0b24352c2555ca06e36b2bf10ed427
Sunil <sunildivase07@gmail.com>
2025-04-28 14:12:01 +0530

    initial import

---
+++ src/main/java/com/springBootHealthcare/healthcareApp/HealthcareApp.java
@@ -0,0 +1,13 @@
+package com.springBootHealthcare.healthcareApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthcareApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthcareApp.class, args);
+	}
+
+}
---
+++ src/test/java/com/springBootHealthcare/healthcareApp/HelathcareAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBootHealthcare.healthcareApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelathcareAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
